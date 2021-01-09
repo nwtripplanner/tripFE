2f9cf31892883765f18753558e23fcca7af4677c
foamyseal <martin.red.202@gmail.com>
2021-01-09 14:51:47 -0800

    merge client and server

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // <img src={logo} className="App-logo" alt="logo" />
+  return (
+    <div className="App">
+      <header className="App-header">
+        <b>
+        <p>
+          <code>Your vacation begins</code>
+        </p>
+        </b>
+        <a
+          className="App-link"
+          // href="https://reactjs.org"
+
+
+        <script>
+        function pageRedirect() {
+        window.location.href = "https://www.tutorialrepublic.com/";
+        }      
+          </script> 
+        <button type="button" onclick="pageRedirect()">Go to Tutorial Republic</button>
+          
+
+
+
+
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <b>
+          <code2>here.</code2>
+          </b>
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
