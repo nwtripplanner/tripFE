c7505eeb20691b259014e876d05ba59e08064d8a
foamyseal <martin.red.202@gmail.com>
2021-01-10 01:36:52 -0800

    basic frontpage, login and register page ui

--- client/src/components/main/Dashboard.js
+++ client/src/components/main/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../login/actions/authActions";
+import SearchBar from "./components/SearchBar";
+import { Box, Grommet } from 'grommet'; 
 
 
 class Dashboard extends Component {
@@ -11,16 +13,28 @@ class Dashboard extends Component {
   };
 render() {
     const { user } = this.props.auth;
+
+    const theme = {
+        global: {
+          font: {
+            family: 'Roboto',
+            size: '18px',
+            height: '20px',
+          },
+        },
+      };
+
 return (
-      <div>
+      <Grommet theme={theme}>
         <div className="row">
-          <div>
+          <Box>
             <h4>
               <b>Hey there,</b> {user.name.split(" ")[0]}
               <p>
                 You are logged 
               </p>
             </h4>
+            <SearchBar />
             <button
               style={{
                 width: "150px",
@@ -32,9 +46,9 @@ return (
             >
               Logout
             </button>
-          </div>
+          </Box>
         </div>
-      </div>
+      </Grommet>
     );
   }
 }
