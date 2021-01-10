fbd6328e21d4c3e6b0cd70a33f46c833b8fcbf03
foamyseal <martin.red.202@gmail.com>
2021-01-09 17:39:17 -0800

    complete user creation and login in server

--- server/server.js
+++ server/server.js
@@ -3,27 +3,34 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+
 const users = require("./routes/api/users");
 
 const app = express();
 const db = require('./db')
 
-// module.exports = mongoose.model("User", UserSchema);
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-app.post("/createtrip", (req, res) => {
-  console.log(req.body);
-  const newTrip = {
-    tripID: req.body.tripID,
-    Name: req.body.tripName,
-    Transportation: req.body.tripTransport,
-    Accomadations: req.body,
-  };
-});
+// // app.post("/createtrip", (req, res) => {
+// //   console.log(req.body);
+// //   const newTrip = {
+// //     tripID: req.body.tripID,
+// //     Name: req.body.tripName,
+// //     Transportation: req.body.tripTransport,
+// //     Accomadations: req.body,
+// //   };
+// // });
 
 app.use(passport.initialize());
 
