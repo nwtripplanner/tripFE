ea35a9d9d422719d8982ccfc6eefaeeb67fe9b2f
Naomi Tran <naaomitran@gmail.com>
2021-01-09 16:53:27 -0800

    main page

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+// import React, { Component } from "react";
+// import { Nav, NavItem} from 'reactstrap';
+// import { NavLink } from 'react-router-dom';
+ 
+// class NavBar extends Component {
+//   render() {
+//     return (
+//         <nav className="navbar navbar-expand-md navbar-light sticky-top" role="navigation">
+//         <div className="container-fluid">
+//             <a className="navbar-brand" href="/home">Brand</a>
+//             <Nav className="ml-auto">
+//               <NavItem>
+//                 <NavLink to="/search" className="nav-link">
+//                   Search
+//                 </NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink to="/login" className="nav-link">
+//                   Login
+//                 </NavLink>
+//               </NavItem>
+//             </Nav>
+//         </div>
+//       </nav>
+//     );
+//   }
+// }
+ 
+// export default NavBar;
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import React from 'react';
+// import React from 'react';
 
 const SearchBar = ({keyword,setKeyword}) => {
-    const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
+    const BarStyling = {width:"35rem",background:"#F2F1F9", opacity: 0.80, border:"none", padding:"1.0rem"};
     return (
       <input 
        style={BarStyling}
        key="random1"
        value={keyword}
-       placeholder={"search country"}
+       placeholder={"Where to?"}
        onChange={(e) => setKeyword(e.target.value)}
       />
     );
